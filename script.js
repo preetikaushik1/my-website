6e3b9c0796477c9e14a2213067b531891ddcbc9a
preeti1098 <124133826+preeti1098@users.noreply.github.com>
2023-05-05 13:03:51 +0530

    my website
    my website

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// text moving code
+var typing = new  Typed(".text",{
+    strings:[ "Preeti Kaushik" , "Frontend Developer" , "Freelancer" , "Web Developer"],
+    typeSpeed:100,
+    backSpeed:40,
+    loop:true,
+});
